71753721d8b047a8afbf7cb77e7dda48a49da066
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2022-11-24 20:00:32 +0200

    Make an actual h file

--- ecs.c
+++ ecs.c
@@ -1,96 +1,14 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdint.h>
-
-#define MAX_ENTITIES 1024
-#define MAX_COMPONENTS 32 // Per entity
-
-static int32_t entityCount = 0; 
-static int32_t availableEntityCount;
-
-static int32_t entities[MAX_ENTITIES];
-static int32_t availableEntityArr[MAX_ENTITIES];
-
-static uint32_t componentArray[MAX_ENTITIES][MAX_COMPONENTS];
-static uint32_t entityComponentCount[MAX_ENTITIES];
-
-void initEntities(void)
-{
-    for (int32_t i = 0; i < MAX_ENTITIES; i++)
-    { 
-        availableEntityArr[i] = i;
-        entities[i] = -1;
-    }
-}
-
-int32_t createEntity(void)
-{
-    if (entityCount >= MAX_ENTITIES)
-    {
-        puts("Error: Cant create more entities!");
-        return -1;
-    }
-    return entityCount++;
-}
-
-int32_t getAvailableEntities(void)
-{
-    int32_t count = 0;
-    for (int32_t i = 0; i < MAX_ENTITIES; i++)
-    {
-        if (entities[i] == -1)
-        {
-            availableEntityArr[count++] = i;
-        }
-    }
-    return count;
-}
-
-void removeEntity(int32_t EntityID)
-{
-    entities[EntityID] = -1;
-    int32_t count = getAvailableEntities();
-    availableEntityArr[count] = EntityID;
-    entityCount--;
-}
-
-typedef enum 
-{
-    NO_COMPONENT = 0,
-    POS_COMPONENT,
-    VEL_COMPONENT,
-    SPRITE_COMPONENT,
-} Component;
-
-typedef struct
-{
-    float x, y;
-} PositionComponent;
-
-void attachComponent(int32_t EntityID, Component component)
-{
-    if (EntityID < 0)
-    {
-        puts("Error: EntityID below 0!");
-        return;
-    }
-    uint32_t componentID = entityComponentCount[EntityID]++;
-    printf("ComponentID = %d\n",componentID);
-    if (componentID >= MAX_COMPONENTS)
-    {
-        printf("Error: Entity ID %d has already reached maximum amount of components!\n", EntityID);
-        return;
-    }
-    componentArray[EntityID][componentID] = component;
-}
+#include "ecs.h"
 
 int main(int argc, char* argv[])
 {
     initEntities();
 
+    int32_t gameEntites[MAX_ENTITIES];
+
     for (int32_t i = 0; i < MAX_ENTITIES ; i++)
     {
-        entities[i] = createEntity();
+        gameEntites[i] = createEntity();
         entityComponentCount[i] = 0;
     }
 
@@ -102,6 +20,14 @@ int main(int argc, char* argv[])
     removeEntity(2);
     count = getAvailableEntities();
     puts("Removed entity 2");
+
+    int32_t asd = createEntity();
+    printf("asdasd: %d\n", asd);
+
+    int32_t asd2 = createEntity();
+    printf("asdasd2: %d\n", asd2);
+
+    count = getAvailableEntities();
 
     printf("Available entity count = %d\n", count);
     printf("First available entity id = %d\n", availableEntityArr[0]);
---
+++ ecs.h
@@ -0,0 +1,109 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAX_ENTITIES 1024
+#define MAX_COMPONENTS 32 // Per entity
+
+static int32_t entityCount = 0;
+static int32_t availableEntityCount;
+
+static int32_t entities[MAX_ENTITIES];
+static int32_t availableEntityArr[MAX_ENTITIES];
+
+static uint32_t componentArray[MAX_ENTITIES][MAX_COMPONENTS];
+static uint32_t entityComponentCount[MAX_ENTITIES];
+
+void initEntities(void)
+{
+    for (int32_t i = 0; i < MAX_ENTITIES; i++)
+    {
+        availableEntityArr[i] = i;
+        entities[i] = -1;
+    }
+}
+
+void updateAvailableEntities(void)
+{
+    int32_t count = 0;
+    for (int32_t i = 0; i < MAX_ENTITIES; i++)
+    {
+        if (entities[i] == -1)
+        {
+            availableEntityArr[count++] = i;
+        }
+    }
+    availableEntityCount = count;
+}
+
+int32_t getAvailableEntities(void)
+{
+    updateAvailableEntities();
+    return availableEntityCount;
+}
+
+int32_t createEntity(void)
+{
+    if (entityCount >= MAX_ENTITIES)
+    {
+        puts("Error: Cant create more entities!");
+        return -1;
+    }
+    printf("EntityID of current EntityCount: %d\n",entities[entityCount]);
+    if (entities[entityCount] != -1)
+    {
+        availableEntityCount = getAvailableEntities();
+        if (availableEntityCount > 0)
+        {
+            entityCount++;
+            int32_t id = availableEntityArr[0];
+            availableEntityArr[0] = -1;
+            printf("Created entity id from createEntity(): %d\n", id);
+            entities[id] = id;
+            updateAvailableEntities();
+            return id;
+        } else return -1;
+    }
+    entityCount++;
+    entities[entityCount] = entityCount;
+    printf("Entitycount from createEntity(): %d\n", entityCount);
+    return entityCount;
+}
+
+void removeEntity(int32_t EntityID)
+{
+    entities[EntityID] = -1;
+    int32_t count = getAvailableEntities();
+    availableEntityArr[count] = EntityID;
+    entityCount--;
+}
+
+typedef enum
+{
+    NO_COMPONENT = 0,
+    POS_COMPONENT,
+    VEL_COMPONENT,
+    SPRITE_COMPONENT,
+} Component;
+
+typedef struct
+{
+    float x, y;
+} PositionComponent;
+
+void attachComponent(int32_t EntityID, Component component)
+{
+    if (EntityID < 0)
+    {
+        puts("Error: EntityID below 0!");
+        return;
+    }
+    uint32_t componentID = entityComponentCount[EntityID]++;
+    printf("ComponentID = %d\n",componentID);
+    if (componentID >= MAX_COMPONENTS)
+    {
+        printf("Error: Entity ID %d has already reached maximum amount of components!\n", EntityID);
+        return;
+    }
+    componentArray[EntityID][componentID] = component;
+}
